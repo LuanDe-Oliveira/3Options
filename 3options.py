3f0389e8ceca08fc075194a54c0b0659decfc397
Luan de Oliveira <luannoliveira421@gmail.com>
2025-02-08 08:24:51 -0300

    Update 3options.py

--- 3options.py
+++ 3options.py
@@ -1,12 +1,22 @@
 import streamlit as st
 
-a = st.toggle('Tem namorada')
-b = st.toggle('É bom em esportes')
-c = st.toggle('É inteligente')
+# Função para controlar o comportamento dos checkboxes
+def controlar_checkboxes():
+    # Checkboxes
+    toggle1 = st.checkbox('Toggle 1')
+    toggle2 = st.checkbox('Toggle 2')
+    toggle3 = st.checkbox('Toggle 3')
+    
+    # Se os dois primeiros toggles forem selecionados, desmarcar o terceiro
+    if toggle1 and toggle2:
+        toggle3 = False
+        st.checkbox('Toggle 3', value=False, disabled=True)
+    
+    # Retornar os estados dos toggles
+    return toggle1, toggle2, toggle3
 
-if a and b:
-    c = st.toggle('É inteligente', value=False, disabled=True)
-elif b and c:
-    a = st.toggle('Tem namorada', value=False, disabled=True)
-elif a and c:
-    b = st.toggle('É bom em esportes', value=False, disabled=True)
+# Título do app
+st.title("Exemplo de Toggles com Streamlit")
+
+# Chamada da função que verifica os checkboxes
+toggle1, toggle2, toggle3 = controlar_checkboxes()
