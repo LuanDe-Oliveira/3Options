aaf66ce3035b0e0e6fc298b7dedc1732a2df8c5c
Luan de Oliveira <luannoliveira421@gmail.com>
2025-02-08 08:15:49 -0300

    Update 3options.py

--- 3options.py
+++ 3options.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-1 = st.toogle('Tem namorada')
-2 = st.toogle('É bom em esportes')
-3 = st.toogle('É inteligente')
+a = st.toogle('Tem namorada')
+b = st.toogle('É bom em esportes')
+c = st.toogle('É inteligente')
 
-if 1,2:
-  3 = st.toogle('É inteligente', disabled=True)
-elif 2,3:
-  1 = st.toogle('Tem namorada', disabled=True)
-elif 1,3:
-  2 = st.toogle('É bom em esportes', disabled=True)
+if a,b:
+  c = st.toogle('É inteligente', disabled=True)
+elif b,c:
+  a = st.toogle('Tem namorada', disabled=True)
+elif a,c:
+  b = st.toogle('É bom em esportes', disabled=True)
