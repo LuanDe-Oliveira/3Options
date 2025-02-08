15c4093c229d259f4cfedd4a4dfba47069de3b1f
Luan de Oliveira <luannoliveira421@gmail.com>
2025-02-08 08:23:21 -0300

    Update 3options.py

--- 3options.py
+++ 3options.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-a = st.toogle('Tem namorada')
-b = st.toogle('É bom em esportes')
-c = st.toogle('É inteligente')
+a = st.toggle('Tem namorada')
+b = st.toggle('É bom em esportes')
+c = st.toggle('É inteligente')
 
 if a and b:
-    c = st.toogle('É inteligente', value=False, disabled=True)
+    c = st.toggle('É inteligente', value=False, disabled=True)
 elif b and c:
-    a = st.toogle('Tem namorada', value=False, disabled=True)
+    a = st.toggle('Tem namorada', value=False, disabled=True)
 elif a and c:
-    b = st.toogle('É bom em esportes', value=False, disabled=True)
+    b = st.toggle('É bom em esportes', value=False, disabled=True)
